c7bdfed8333edfa514cf393aa8c756e5d25d0e4a
Mychailo Sobko <supermike5052@gmail.com>
2019-02-04 01:40:02 +0000

    Created markup

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const a = parseInt(prompt('Enter your index'));
+
+const fibNumbers = [1, 1];
+
+const ourNumber = checkNumber(a);
+alert(ourNumber);
+
+function checkNumber() {
+    if (a === 1) {
+        return 'Your number is ' + 1;
+    }
+    if (a === 2) {
+        return 'Your number is ' + 1;
+    }
+    let i = 3;
+    while (i <= a) {
+        fibNumbers.push(fibNumbers[i - 3] + fibNumbers[i - 2]);
+        i++;
+    }
+    return 'Your number is ' + fibNumbers[a - 1];
+}
